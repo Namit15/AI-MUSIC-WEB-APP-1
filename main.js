e5b3912e8770c0c31453e7e0a434630bc7321ca5
Namit15 <92371924+Namit15@users.noreply.github.com>
2022-11-25 20:18:12 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 song = "";
 song1 = "";
 
+songStatus = "";
+songStatus1 = "";
+
 leftWristX = 0;
 leftWristY = 0;
 
@@ -8,7 +11,7 @@ rightWristX = 0;
 rightWristY = 0;
 
 scoreRightWrist = 0;
-scoreLeftWrist - 0;
+scoreLeftWrist = 0;
 
 function setup()
 {
@@ -20,19 +23,26 @@ function setup()
     poseNet.on("pose", gotPoses);
 }
 
+function modelloadded()
+{
+    console.log("Your model is lodded")
+}
+
 
 function gotPoses(results)
 {
     if(results.length > 0)
     {
         console.log(results);
-        leftWristX = results[0].pose.leftWrist.X;
-        leftWristY = results[0].pose.leftWrist.Y;
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist  : " + scoreLeftWrist);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
 
         console.log("Left Wrist X :" + leftWristX + "Left Wrist Y :" + leftWristY);
 
-        rightWristX = results[0].pose.rightWrist.X;
-        rightWristY = results[0].pose.rightWrist.leftWristY;
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
 
         console.log("Right Wrist X :" + rightWristX + "Right Wrist Y :" + rightWristY);
     }
@@ -47,9 +57,28 @@ function preload()
 function draw()
 {
     image(video,0,0,600,500);
+
+    songStatus = song.isPlaying();
+    songStatus1 = song1.isPlaying();
+
+    fill("red");
+    stroke("red");
+
+    if(scoreLeftWrist > 0.2)
+    {
+        circle(leftWristX,leftWristY,20);
+        song.stop();
+
+        if(songStatus1 == false)
+        {
+            song1.play();
+        }
+    }
 }
 
 function play()
 {
     song.play();
+    song.setVolume(1);
+    song.rate(1);
 }
